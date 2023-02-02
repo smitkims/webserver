09c1c7c1933422e0d6a660ad54e33172fedff939
나예은 <63445400+smitkims@users.noreply.github.com>
2023-02-02 13:33:21 +0900

    Update server.c

--- server.c
+++ server.c
@@ -24,29 +24,6 @@ char usrname[] = "username";
 char password[] = "password";
 #include <stdint.h>
 #include <stdlib.h>
-//Problem 1 of project 1:simple webserver with authentification
-//Both Problem 1 and 2 was tested on WSL enviroments, Linux, and M1 mac
-//But If you still have problems on running codes please mail us
-//Most importantly please comment your code
-
-//If you are using mac 
-//You can install homebrew here :https://brew.sh
-//And open terminal and type 
-//sudo brew install gcc
-//sudo brew install make
-//Type make command to build server
-//And server binary will be created
-//Use ifconfig command to figure out your ip(usually start with 192. or 172.)
-//run server with ./server and open browser and type 192.x.x.x:25000
-
-//If you are using Linux or WSL
-//You just need to run "make"(If you are using WSL you may need to install gcc and make with apt)
-//And server binary will be created
-//Use ifconfig command to figure out your ip(usually start with 192. or 172.)
-//run server with ./server and open browser and type 192.x.x.x:25000
-
-//It will be better if you run virtual machine or other device to run server
-//But you can also test server with opening terminal and run it on local IP 
 
 int main( int argc, char *argv[] ) {
   int sockfd, newsockfd, portno = PORT;
@@ -54,12 +31,9 @@ int main( int argc, char *argv[] ) {
   struct sockaddr_in serv_addr, cli_addr;
   clilen = sizeof(cli_addr);
 
-  printf("encoding start \n");// We have implemented base64 encoding you just need to use this function
+  printf("encoding start \n");
   char *token = base64_encode("2018-13749:password", strlen("2018-13749:password"));//you can change your userid
   printf("encoding end \n");
-
-  //browser will repond with base64 encoded "userid:password" string 
-  //You should parse authentification information from http 401 responese and compare it
 
   /* First call to socket() function */
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -82,15 +56,12 @@ int main( int argc, char *argv[] ) {
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(portno);
-  /* TODO : Now bind the host address using bind() call. 10% of score*/
-    //it was mostly same as tutorial
 
     // bind socket to the specified IP and port
   if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
     perror("bind error");
     exit(1);
   }
-  /* TODO : listen on socket you created  10% of score*/
   //listen for connections
   if(listen(sockfd, 10) == -1){
     perror("listen error");
@@ -98,29 +69,19 @@ int main( int argc, char *argv[] ) {
   }
 
   printf("Server is running on port %d\n", portno);
-    
-    //it was mostly same as tutorial
-    //in the while loop every time request comes we respond with respond function if valid
 
-    //TODO: authentication loop 40 % of score
     while(1){
       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-      //TODO: accept connection
-      //TODO: send 401 message(more information about 401 message : https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication) and authentificate user
       //close connection
       if (newsockfd == -1 ){
           perror("accept error");
           exit(1);
         }
-        
-        //browser will repond with base64 encoded "userid:password" string 
-        //You should parse authentification information from http 401 responese and compare it
-        //respond(newsockfd);
+        respond(newsockfd);
       }
 
   return 0;
 }
-//TODO: complete respond function 40% of score
 // fopen, strtok
 int respond(int sock) {
   int offset, bytes;
